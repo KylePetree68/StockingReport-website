7a9770e2347c5f1db77ab7a8ccb8c532d7156fa0
KylePetree68 <kyle@kissmygrits.net>
2025-07-19 16:29:23 -0600

    Update build.sh new build no duplicates scraper

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 #python rebuild_database.py
-# python scraper.py
+python scraper.py
 #python scraper.py --rebuild
-python clean_database.py
+#python clean_database.py

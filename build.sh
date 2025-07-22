bfa2b8820d5c7868958aa79af22937d6e52955ba
KylePetree68 <kyle@kissmygrits.net>
2025-07-22 12:00:20 -0600

    Update build.sh --rebuild

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 #python scraper.py --rebuild
 #python clean_database.py
 #
-mkdir -p public/waters
 pip install -r requirements.txt
-python scraper.py
+#mkdir -p public/waters
+#python scraper.py
+python scraper.py --rebuild
